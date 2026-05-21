1fcd604ca14170a76a325a56b6482889161913ee
kevalkhetani151 <kevalkhetani15@gmail.com>
2026-05-21 13:29:09 +0530

    change page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,8 +1,5 @@
-import { redirect } from 'next/navigation';
-
-export const dynamic = 'force-dynamic';
 
 // The root of the dashboard route group redirects to /dashboard
 export default function DashboardRoot() {
-  redirect('/home');
+  return <h1> this is a dashboard</h1>
 }
